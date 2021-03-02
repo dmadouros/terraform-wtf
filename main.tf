dac93e40614e42f64301da9ab4c76cb66e44e3a8
David Madouros <david@earthvectors.com>
2021-03-02 00:19:47 -0700

    Initial commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-1"
+}
+
+output "hello" {
+  value = "world"
+}
+
